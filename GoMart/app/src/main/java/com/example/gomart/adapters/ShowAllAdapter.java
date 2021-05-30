f95909c0073917cca46048f9045b12d562ff5fa4
mcsf19m0004 <inshalbaba111@gmail.com>
2021-05-30 13:11:21 +0500

    gomart

--- GoMart/app/src/main/java/com/example/gomart/adapters/ShowAllAdapter.java
+++ GoMart/app/src/main/java/com/example/gomart/adapters/ShowAllAdapter.java
@@ -1,5 +1,6 @@
 package com.example.gomart.adapters;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -14,51 +15,61 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.gomart.R;
 import com.example.gomart.activities.DetailedActivity;
+import com.example.gomart.models.NewProoductsModel;
+import com.example.gomart.models.ShowAllModel;
 
 import java.util.List;
 
-public class ShowAllAdapter  extends RecyclerView.Adapter<ShowAllAdapter.ViewHolder> {
+public class ShowAllAdapter extends RecyclerView.Adapter<ShowAllAdapter.ViewHolder> {
+
 
     private Context context;
-    private List<ShowAllAdapter> list;
+    private List<ShowAllModel> list;
 
+    public ShowAllAdapter(Context context, List<ShowAllModel> list)
+    {
+        this.context=context;
+        this.list=list;
+    }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_show_all,parent,false));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.show_all_item,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Glide.with(context).load(list.get(position).getImg_url()).into(holder.mItemImage);
-        holder.newName.setText(list.get(position).getName());
-        holder.newPrice.setText(String.valueOf(list.get(position).getPrice()));
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        Glide.with(context).load(list.get(position).getImg_url()).into(holder.mItemImg);
+        holder.mName.setText(list.get(position).getName());
+        holder.mCost.setText(String.valueOf(list.get(position).getPrice()));
+
+      /*  holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(context, DetailedActivity.class);
                 intent.putExtra("detailed", list.get(position));
                 context.startActivity(intent);
             }
-        });
+        });*/
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-      private   ImageView mItemImg;
-       private TextView mName, mCost;
 
+        private ImageView mItemImg;
+        private TextView mCost,mName;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             mItemImg=itemView.findViewById(R.id.item_image);
-            mName=itemView.findViewById(R.id.item_nam);
             mCost=itemView.findViewById(R.id.item_cost);
+            mName=itemView.findViewById(R.id.item_nam);
         }
     }
 }
--- GoMart/app/src/main/java/com/example/gomart/models/ShowAllModel.java
+++ GoMart/app/src/main/java/com/example/gomart/models/ShowAllModel.java
@@ -2,7 +2,7 @@ package com.example.gomart.models;
 
 import java.io.Serializable;
 
-public class ShowAllModel implements Serializable {
+public class ShowAllModel  {
 
     String description;
     String name;
@@ -10,10 +10,9 @@ public class ShowAllModel implements Serializable {
     int price;
     String img_url;
 
-    public ShowAllModel()
-    {
-
+    public ShowAllModel() {
     }
+
     public ShowAllModel(String description, String name, String rating, int price, String img_url) {
         this.description = description;
         this.name = name;
@@ -22,44 +21,44 @@ public class ShowAllModel implements Serializable {
         this.img_url = img_url;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setRating(String rating) {
-        this.rating = rating;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setImg_url(String img_url) {
-        this.img_url = img_url;
-    }
-
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getRating() {
         return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
     }
 
     public int getPrice() {
         return price;
     }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public String getImg_url() {
         return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
     }
 }
 
